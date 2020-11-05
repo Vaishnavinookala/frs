6d82529d4d25a88f0afdd76247a267b8cb211c5c
Ishwar Soni <ishwar.soni@upgrad.com>
2020-11-05 11:18:59 +0530

    checkpoint 2

---
+++ upgrad-frs/src/com/upgrad/frs/Flight.java
@@ -0,0 +1,28 @@
+package com.upgrad.frs;
+
+public class Flight {
+    String flightNumber;
+    String airline;
+    int capacity;
+    int bookedSeats;
+
+    Flight(String flightNumber, String airline, int capacity, int bookedSeats) {
+        this.flightNumber = flightNumber;
+        this.airline = airline;
+        this.capacity = capacity;
+        this.bookedSeats = bookedSeats;
+    }
+
+    String getFlightDetails() {
+        return "Flight No: " + flightNumber + ", Airline: " + airline +
+                ", Capacity: " + capacity + ", Booked Seats: " + bookedSeats;
+    }
+
+    boolean checkAvailability() {
+        return bookedSeats < capacity;
+    }
+
+    void incrementBookingCounter() {
+        bookedSeats++;
+    }
+}
