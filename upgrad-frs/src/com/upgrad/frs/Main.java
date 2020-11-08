097aacde763174cae93af5343983b4e28910e1de
Ishwar Soni <ishwar.soni@upgrad.com>
2020-11-08 17:51:44 +0530

    checkpoint 5

--- upgrad-frs/src/com/upgrad/frs/Main.java
+++ upgrad-frs/src/com/upgrad/frs/Main.java
@@ -3,6 +3,21 @@ package com.upgrad.frs;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Ticket regularTicket = new RegularTicket("12345", "Bengaluru",
+                "Delhi", "DepartureDateTime", "ArrivalDateTime",
+                "6F", 5000, false, null, null, "lunch");
+
+        Ticket touristTicket = new TouristTicket("67890", "India", "Dubai",
+                "DepartureDateTime", "ArrivalDateTIme",
+                "10A", 15000, false, null, null,
+                "Dubai Hotel", new String[]{""});
+
+        printTicketDetails(regularTicket);
+        printTicketDetails(touristTicket);
     }
+
+    public static void printTicketDetails(Ticket ticket) {
+        System.out.println(ticket.getPnr());
+    }
+
 }
